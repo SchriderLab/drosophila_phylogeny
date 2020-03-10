e91bb7e31472ed43dc0ad537370994c8a372b7a9
Antons <antony.suvorov@gmail.com>
2020-03-09 22:52:06 -0400

    update

--- scripts/phylonet_nexus_generate.r
+++ scripts/phylonet_nexus_generate.r
@@ -56,8 +56,61 @@ J=c('D_arawakana','D_dunni','D_cardini','D_ornatifrons','D_subbadia','D_pallidip
 K=c('D_albomicans','D_nasuta','D_kepulauana','D_neonasuta','D_sulfurigaster_albostrigata','D_pulaua','D_sulfurigaster_bilimbata','D_sulfurigaster_sulfurigaster','D_neohypocausta','D_immigrans','D_immigrans_kari17','D_pruinosa')
 
 
+C1=c('D_acanthoptera','D_wassermani','D_pachea','D_nannoptera','D_bromeliae')
 
-phylonet_chunk_topo=function(sp_tree,gene_trees,sp_list,n_retic)
+C2=c('D_arizonae','D_mojavensis','D_navojoa','D_seriema','D_repleta','D_hydei','D_pseudotalamancana')
+
+C3=c('D_americana','D_novamexicana','D_lummei','D_virilis','D_montana','D_robusta')
+
+C4=c('D_cyrtoloma','D_differens','D_planitibia','D_heteroneura','D_silvestris')
+
+C5=c('D_grimshawi','D_limitata','D_ochracea','D_villosipedis','D_murphyi','D_sproati','D_paucipunta','D_prolacticillia','D_primaeva')
+
+C6=c('S_elmoi','S_pallida','S_flava','S_montana')
+
+C7=c('D_albomicans','D_nasuta','D_kepulauana','D_neonasuta','D_sulfurigaster_albostrigata','D_pulaua','D_sulfurigaster_bilimbata','D_sulfurigaster_sulfurigaster')
+
+C8=c('D_neohypocausta','D_immigrans','D_immigrans_kari17','D_pruinosa')
+
+C9=c('D_arawakana','D_dunni','D_cardini','D_ornatifrons','D_subbadia','D_pallidipennis')
+    
+C10=c('D_funebris','D_guttifera','D_innubila','D_mush_saotome','D_quadrilineata')
+
+C11=c('Z_africanus','Z_gabonicus','Z_indianus_16GNV01')
+
+C12=c('Z_camerounensis','Z_nigranus','Z_lachaisei','Z_vittiger','Z_capensis','Z_davidi','Z_taronus','Z_ornatusmayotte')
+
+C13=c('Z_ghesquierei','Z_inermis','Z_kolodkinae','Z_sepsoides_mayotte','Z_tsacasi','Z_tuberculatus')
+
+
+
+C14=c('D_affinis','D_athabasca','D_azteca','D_lowei','D_miranda','D_persimilis','D_pseudoobscura')
+C15=c('D_bifasciata','D_obscura','D_guanche','D_subobscura')
+C16=c('D_ananassae','D_parapallidosa','D_bipectinata','D_parabipectinata','D_m_pallens','D_malerkotliana_malerkotliana','D_p_nigrens','D_pseudoananassae_pseudoananassae','D_ercepeae')
+C17=c('D_asahinai','D_lacteicornis','D_rufa','D_tani','D_aurauria','D_triauraria','D_pectinifera')
+C18=c('D_bakoue','D_burlai','D_montium_STUp','D_montium_STLow','D_nikananu','D_seguyi','D_vulcana','D_jambulina')
+C19=c('D_birchii','D_mayri','D_truncata','D_bunnanda','D_serrata','D_punjabiensis','D_watanabei','D_kikkawai','D_leontia','D_kanapiae')
+
+C20=c('D_biarmipes','D_subpulchrella','D_suzukii','D_takahashii')
+C21=c('D_erecta','D_orena','D_teissieri_273_3','D_yakuba','D_mauritiana','D_simulans','D_sechellia','D_melanogaster','D_eugracilis')
+C22=c('D_carrolli','D_rhopaloa','D_kurseongensis','D_fuyamai','D_elegans','D_ficusphila')
+C23=c('D_equinoxialis','D_paulistorum','D_paulistorum_L12','D_willistoni','D_tropicalis','D_insularis')
+C24=c('D_neocordata','D_prosaltans','D_saltans','D_sturtevanti','L_magnipectinata','L_stackelbergi')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+phylonet_chunk_topo=function(sp_tree,gene_trees,sp_list,n_retic,filename)
 {
     sp_phy=read.tree(sp_tree)
     sp_phy$node.label=NULL
@@ -67,41 +120,45 @@ phylonet_chunk_topo=function(sp_tree,gene_trees,sp_list,n_retic)
     rooted_phy=c()
     for (gt in phy)
     {
-        if (any(sp_list %in% gt$tip.label) & any(out_v %in% gt$tip.label))
+        if (any(sp_list %in% gt$tip.label) & any(out_v %in% gt$tip.label) & sum(sp_list %in% gt$tip.label)>=3)
         {
-            if (is.monophyletic(gt,sp_list[sp_list %in% gt$tip.label])) 
-            {
+            
                 
-                out_sp=out_v[which(out_v %in% gt$tip.label)[1]]
-                gt_r=root(gt,out_sp,resolve.root=T)
-                gt_sub=keep.tip(gt_r,c(sp_list[sp_list %in% gt$tip.label],out_v))
-                gt_sub$node.label[gt_sub$node.label=="Root"]=100
-                gt_sub$node.label=as.numeric(gt_sub$node.label)/100
-                rooted_phy=c(rooted_phy,write.tree(gt_sub))
-            }    
+            out_sp=out_v[which(out_v %in% gt$tip.label)[1]]
+            gt_r=root(gt,out_sp,resolve.root=T)
+            gt_sub=keep.tip(gt_r,c(sp_list[sp_list %in% gt$tip.label],out_v))
+            gt_sub$node.label[gt_sub$node.label=="Root"]=100
+            gt_sub$node.label=as.numeric(gt_sub$node.label)/100
+            rooted_phy=c(rooted_phy,write.tree(gt_sub))
+                
         } 
         
     } 
     phy=rooted_phy
     for (i in 1:n_retic)
     {    
-        filename=deparse(substitute(sp_list))
+        #filename=deparse(substitute(sp_list))
         f_n=paste(filename,"_phylonet_genes_",i,"ret.nex",sep="")
         write("#NEXUS\n\nBEGIN TREES;",f_n)
         write(paste("Tree fixtr = ",write.tree(sp_phy_sub)),f_n,append=TRUE)
         d=data.frame(rep("Tree",length(phy)),paste("gt",1:length(phy),"=",sep=""),phy)
         write.table(d,f_n, quote = FALSE,row.names = FALSE, col.names=FALSE,append=TRUE)
         write("END;\n\nBEGIN PHYLONET;",f_n,append=TRUE)
-        write(paste("InferNetwork_ML (all)",i,"-s fixtr -di -pl 10 -x 10 -b 0.9 -po;","\nEND;"),f_n,append=TRUE)
+        write(paste("InferNetwork_ML (all)",i,"-s fixtr -di -pl 10 -x 30 -b 0.9 -po;","\nEND;"),f_n,append=TRUE)
     }    
 }    
 
-phylonet_chunk_topo("MLrooted.tre","gene_trees_wboot_dna_mafft",I,1)
 
-for (i in list(A,B,C))
+my_clade_list=list(C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,C14,C15,C16,C17,C18,C19,C20,C21,C22,C23,C24)
+names(my_clade_list)=paste("C",1:24,sep="")
+for (i in 1:length(my_clade_list))
 {
-    phylonet_chunk_topo("MLrooted.tre","gene_trees_wboot_dna_mafft",i,1)
+    phylonet_chunk_topo("MLrooted.tre","gene_trees_wboot_dna_mafft",my_clade_list[[i]],3,names(my_clade_list[i]))
 }    
+
+
+
+
 
 
 
