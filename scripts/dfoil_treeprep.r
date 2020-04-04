33025b1d9610428b7f189d6a1b62eccfce1eedb1
Antons <antony.suvorov@gmail.com>
2020-04-04 06:14:29 -0400

    update

---
+++ scripts/dfoil_treeprep.r
@@ -0,0 +1,38 @@
+library("ape")
+library("phytools")
+
+
+
+extract.clade(tt, 245)
+ combn(zz$tip.label,m=5)
+
+tt=read.tree("MLrooted.tre")
+cl1=extract.clade(tt, 245)
+
+dfoil_select=function(phy)
+{
+    dfoil_out=c()
+    taxa_combn=combn(phy$tip.label,m=5)
+    test_phy=read.tree(text="((('A','A'),('A','A')),A);")
+    for (i in 1:ncol(taxa_combn))
+    {
+        sub_phy=keep.tip(phy,taxa_combn[,i])
+        sub_phy_dup=sub_phy
+        sub_phy_dup$tip.label=rep("A",length(sub_phy_dup$tip.label))
+        if (all.equal.phylo(sub_phy_dup,test_phy,use.edge.length=F,use.tip.label=F))
+        {
+            node_number_order=rev(order(node.depth.edgelength(sub_phy)[(length(sub_phy$tip.label)+1):length(node.depth.edgelength(sub_phy))])+length(sub_phy$tip.label))[1:2]
+            taxa_order=c()
+            for (n in node_number_order)
+            {
+               node_taxa=extract.clade(sub_phy,n)$tip.label
+               taxa_order=c(taxa_order,node_taxa) 
+            }
+            dfoil_order=c(taxa_order,sub_phy$tip.label[!sub_phy$tip.label %in% taxa_order])
+            dfoil_out=c(dfoil_out,paste(dfoil_order,collapse=","))
+            
+        }    
+        
+    }
+    write(dfoil_out,"dfoil_clades")
+}    
