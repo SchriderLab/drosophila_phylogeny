7ae121a1f84d4a6f1966370f8af10a9c52a148e4
Antons <antony.suvorov@gmail.com>
2020-04-16 20:42:08 -0400

    update

--- scripts/dfoil_treeprep.r
+++ scripts/dfoil_treeprep.r
@@ -61,3 +61,26 @@ for (i in node_n)
    dfoil_select_all(extract.clade(tt,i),paste("C",cl_id,"_node",i,"_dfoil_all",sep=""))
    cl_id=cl_id+1 
 }    
+
+
+################################################################ RUN DFOIL ######################################################################## 
+
+
+names_v=c("clade","P1","P2","P3","P4","Out","chrom1","position", "AAAAA" , "AAABA" , "AABAA" , "AABBA" , "ABAAA" , "ABABA" , "ABBAA" , "ABBBA" , "BAAAA" , "BAABA" , "BABAA" , "BABBA" , "BBAAA" ,"BBABA","BBBAA","BBBBA",'chromdup','coord','total','dtotal','T12','T34','T1234','DFO_left','DFO_right','DFO_total','DFO_stat','DFO_chisq','DFO_Pvalue','DIL_left','DIL_right','DIL_total','DIL_stat','DIL_chisq','DIL_Pvalue','DFI_left','DFI_right','DFI_total','DFI_stat','DFI_chisq','DFI_Pvalue','DOL_left','DOL_right','DOL_total','DOL_stat','DOL_chisq','DOL_Pvalue','introgression','introgna','intrognone','introg13','introg14','introg23','introg24','introg31','introg41','introg32','introg42','introg123','introg124')
+
+total=read.csv("/Users/Anton/Downloads/droso_dfoil_results.txt")
+names(total)=names_v
+total=total[total$T12<total$T34,]
+
+total$Genus="Drosophila"
+total$introgressionid=ifelse(total$introgression=="none","None",
+                             ifelse(total$introgression=="123" | total$introgression=="124","Ancestral","Inter-group"))
+
+
+
+total_order=melt(total[,c("introgressionid","Genus")])
+g1=ggplot(total_order, aes(x=Genus, y=..count../sum(..count..),fill=introgressionid))+geom_bar(position="fill")+labs(x="Genus", y = "")+scale_fill_manual(values=c("#f0ad4e", "#5cb85c","#337ab7"),name="Introgression")
+total_suborder=melt(total[,c("introgressionid","clade")])
+g2=ggplot(total_suborder, aes(x=clade, y=..count../sum(..count..),fill=introgressionid))+geom_bar(position="fill")+geom_bar(position="fill")+labs(x="Clade", y = "Proportion of Quintets")+scale_fill_manual(values=c("#f0ad4e", "#5cb85c","#337ab7"),name="Introgression")
+
+grid.arrange(g1,g2,nrow=3)
--- scripts/quible_generate.r
+++ scripts/quible_generate.r
@@ -1,24 +1,178 @@
 library("ape")
 
-quible_trees=function(triplet,gene_trees)
+
+
+.write.tree2 <- function(phy, digits = 10, tree.prefix = "", check_tips)
 {
-    phy=read.tree(gene_trees)
-    trees=c()
-    for (gt in phy)
+    brl <- !is.null(phy$edge.length)
+    nodelab <- !is.null(phy$node.label)
+    #if (check_tips) phy$tip.label <- checkLabel(phy$tip.label)
+    #if (nodelab) phy$node.label <- checkLabel(phy$node.label)
+    f.d <- paste("%.", digits, "f", sep = "")
+    cp <- function(x){
+        STRING[k] <<- x
+        k <<- k + 1
+    }
+    add.internal <- function(i) {
+        cp("(")
+        desc <- kids[[i]]
+        for (j in desc) {
+            if (j > n) add.internal(j)
+            else add.terminal(ind[j])
+            if (j != desc[length(desc)]) cp(",")
+        }
+        cp(")")
+        if (nodelab && i > n) cp(phy$node.label[i - n]) # fixed by Naim Matasci (2010-12-07)
+        if (brl) {
+            cp(":")
+            cp(sprintf(f.d, phy$edge.length[ind[i]]))
+        }
+    }
+    add.terminal <- function(i) {
+        cp(phy$tip.label[phy$edge[i, 2]])
+        if (brl) {
+            cp(":")
+            cp(sprintf(f.d, phy$edge.length[i]))
+        }
+    }
+
+    n <- length(phy$tip.label)
+
+    ## borrowed from phangorn:
+    parent <- phy$edge[, 1]
+    children <- phy$edge[, 2]
+    kids <- vector("list", n + phy$Nnode)
+    for (i in 1:length(parent))
+        kids[[parent[i]]] <- c(kids[[parent[i]]], children[i])
+
+    ind <- match(1:max(phy$edge), phy$edge[, 2])
+
+    LS <- 4*n + 5
+    if (brl) LS <- LS + 4*n
+    if (nodelab)  LS <- LS + n
+    STRING <- character(LS)
+    k <- 1
+    cp(tree.prefix)
+    cp("(")
+    getRoot <- function(phy)
+        phy$edge[, 1][!match(phy$edge[, 1], phy$edge[, 2], 0)][1]
+    root <- getRoot(phy) # replaced n+1 with root - root has not be n+1
+    desc <- kids[[root]]
+    for (j in desc) {
+        if (j > n) add.internal(j)
+        else add.terminal(ind[j])
+        if (j != desc[length(desc)]) cp(",")
+    }
+
+    if (is.null(phy$root.edge)) {
+        cp(")")
+        if (nodelab) cp(phy$node.label[1])
+        cp(";")
+    }
+    else {
+        cp(")")
+        if (nodelab) cp(phy$node.label[1])
+        cp(":")
+        cp(sprintf(f.d, phy$root.edge))
+        cp(";")
+    }
+    paste(STRING, collapse = "")
+}
+
+assignInNamespace(".write.tree2", .write.tree2, "ape")
+
+
+keep.tip.new=function(phy, tip) 
+{
+    if (!inherits(phy, "phylo")) 
+        stop("object \"phy\" is not of class \"phylo\"")
+    Ntip <- length(phy$tip.label)
+    if (is.character(tip)) 
     {
-        if (all(triplet %in% gt$tip.label))
+        idx <- match(tip, phy$tip.label)
+        if (!anyNA(idx)) 
         {
-            gt$node.label=NULL
-            gt_sub=keep.tip(gt,triplet)
-            trees=c(trees,write.tree(gt_sub))
-        }    
-        
-    }    
-    write.table(trees,"quiblin", quote = FALSE,row.names = FALSE, col.names=FALSE,append=TRUE)
+            tip <- idx
+            toDrop <- setdiff(1:Ntip, tip)
+            drop.tip(phy, toDrop)
+        }
+    }
+}
+
+
+
+
+
+all_triplets=function(taxa_list,gene_trees,dir_name)
+{
+    dir.create(dir_name)
+    setwd(dir_name)
+    phy=read.tree(gene_trees)
+    taxa_combn=combn(taxa_list,m=3)
+    for (i in 1:ncol(taxa_combn))
+    {
+        triplet=taxa_combn[,i]
+        sub_triplet=lapply(phy,keep.tip.new, c(triplet,"Anopheles_gambiae"))
+        sub_triplet=Filter(Negate(is.null),sub_triplet)
+        config="
+[Input]
+treefile: ./INNAME
+numdistributions: 2
+likelihoodthresh: 0.01
+numsteps: 50
+gradascentscalar: 0.5
+totaloutgroup: Anopheles_gambiae
+multiproc: True
+maxcores:1000
+[Output]
+OutputPath: ./OUTNAME" 
+        config=gsub("INNAME",paste("quibltrees_",i,sep=""),config)
+        config=gsub("OUTNAME",paste("quibl_out",i,sep=""),config)
+        write(config,paste("quiblin_",i,sep=""))
+        write(unlist(lapply(sub_triplet,write.tree)),paste("quibltrees_",i,sep=""))
     
+    }    
+    setwd("..")
 }    
 
-C16=c('D_ananassae','D_parapallidosa','D_bipectinata','D_parabipectinata','D_m_pallens','D_malerkotliana_malerkotliana','D_p_nigrens','D_pseudoananassae_pseudoananassae','D_ercepeae')
 
-C16_ret=c("Anopheles_gambiae","D_p_nigrens","D_m_pallens","D_parabipectinata")
-quible_trees(C16_ret,"gene_trees_wboot_dna_mafft")
+
+tt=read.tree("MLrooted.tre")
+node_n=c(168,185,204,226,245,256,265,289,307)
+
+
+
+
+all_triplets(extract.clade(tt,168)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",1,"_node",168,"_quibl",sep=""))
+all_triplets(extract.clade(tt,185)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",2,"_node",185,"_quibl",sep=""))
+all_triplets(extract.clade(tt,204)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",3,"_node",204,"_quibl",sep=""))
+all_triplets(extract.clade(tt,226)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",4,"_node",226,"_quibl",sep=""))
+all_triplets(extract.clade(tt,245)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",5,"_node",245,"_quibl",sep=""))
+all_triplets(extract.clade(tt,256)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",6,"_node",256,"_quibl",sep=""))
+all_triplets(extract.clade(tt,265)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",7,"_node",265,"_quibl",sep=""))
+all_triplets(extract.clade(tt,289)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",8,"_node",289,"_quibl",sep=""))
+all_triplets(extract.clade(tt,307)$tip.label,"/Users/Anton/Downloads/gene_trees_wboot_dna_mafft",paste("C",9,"_node",307,"_quibl",sep=""))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
